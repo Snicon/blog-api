d011f72d2c6ef5422b8babbb5f25de9d6e12ec30
Sixten Peterson <sixten.peterson@winterzone.se>
2022-04-29 22:08:38 +0200

    added PostFactory + uuid and slug assignment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public static function booted()
+    {
+        static::creating(function (Post $post) {
+            $post->uuid = Str::uuid();
+
+            if(!$post->slug) {
+                $post->slug = $post->uuid;
+            }
+        });
+    }
 
     protected $fillable = [
         'uuid',
