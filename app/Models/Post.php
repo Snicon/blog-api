456cdda9d20b5350670c7bb602bd4ef621e53d83
Sixten Peterson <sixten.peterson@winterzone.se>
2022-04-29 21:58:46 +0200

    added fillables to the post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'title',
+        'slug',
+        'body',
+        'teaser',
+        'published',
+    ];
 }
