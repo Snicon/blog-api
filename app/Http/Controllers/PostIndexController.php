fffd0f4e92c38094ceb26417bb8a140c31edefd3
Sixten Peterson <sixten.peterson@winterzone.se>
2022-04-30 15:50:11 +0200

    Added PostIndexController + PostResource + route

---
+++ app/Http/Controllers/PostIndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostIndexController extends Controller
+{
+    public function __invoke() {
+
+        return PostResource::collection(Post::wherePublished(true)->get());
+
+    }
+}
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => $this->uuid,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'teaser' => $this->teaser,
+            'body' => $this->body,
+            'published' => $this->published,
+        ];
+    }
+}
